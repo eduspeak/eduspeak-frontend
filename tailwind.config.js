1fb660453a74b1188ce9634536ac82941f831520
Fahmii19 <fhmialfi79@gmail.com>
2023-08-01 18:30:54 +0700

    first commit install react and install flowbite

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./src/**/*.{html,js}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("flowbite/plugin")],
+};
