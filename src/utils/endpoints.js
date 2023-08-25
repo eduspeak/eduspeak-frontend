49cbd6d63927732778feecac51334dcd22aad1db
Fahmii19 <fhmialfi79@gmail.com>
2023-08-26 00:34:28 +0700

    push run build

--- src/utils/endpoints.js
+++ src/utils/endpoints.js
@@ -1,7 +1,7 @@
-const BASE_URL = "http://103.250.11.209:8080/api/course";
+const BASE_URL = "http://103.250.11.209:8080/api";
 
 const Endpoint = {
-  Feedback: `${BASE_URL}/feedback`,
+  Feedback: `${BASE_URL}/course/feedback`,
 };
 
 export default Endpoint;
